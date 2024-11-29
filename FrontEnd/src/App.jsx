90bbf81df7eb9d6810d868b7f8c0f383c174270a
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-29 22:02:13 +0530

    new commits

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
-  return <div>App</div>;
+  return <div>{routes}</div>;
 };
 
 export default App;
