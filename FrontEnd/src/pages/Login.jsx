ca906e40eca3391c8acc1a76c26e0cb630f816e3
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-30 15:53:25 +0530

    new commit

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import { Link } from "react-router-dom";
 import PasswordInput from "../components/PasswordInput.jsx";
+import { validateEmail } from "../utilis/helper.js";
+
 const Login = () => {
-  const [Email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [Error, setError] = useState(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please Enter a valid Email address ");
+      return;
+    }
+    if (!Password) {
+      setError("Please Enter a valid Password ");
+      return;
+    }
+    setError("");
   };
 
   return (
@@ -17,13 +28,13 @@ const Login = () => {
 
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form onSubmit={handleLogin()}>
-            <h4 className="text-2xl mb-7">LogIn</h4>
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7 text-center">LogIn</h4>
             <input
               type="text"
               placeholder="Email"
               className="input-box"
-              value={Email}
+              value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
 
@@ -31,6 +42,8 @@ const Login = () => {
               value={Password}
               onChange={(e) => setPassword(e.target.value)}
             />
+
+            {Error && <p className="text-red-500 text-xs pb-1 ">{Error}</p>}
 
             <button type="submit" className="btn-primary">
               Login
--- FrontEnd/src/pages/SignUp.jsx
+++ FrontEnd/src/pages/SignUp.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar.jsx";
+import { Link } from "react-router-dom";
+import PasswordInput from "../components/PasswordInput.jsx";
+import { validateEmail } from "../utilis/helper.js";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [Name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Error, setError] = useState(null);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!Name) {
+      setError("Name is required");
+    }
+    if (!validateEmail(email)) {
+      setError("Valid email is required");
+    }
+    if (!Password) {
+      setError("Password is required");
+    }
+    setError("");
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7 text-center">SignUp</h4>
+
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={Name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <PasswordInput
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {Error && <p className="text-red-500 text-xs pb-1 ">{Error}</p>}
+
+            <button type="submit" className="btn-primary">
+              SignUp
+            </button>
+            <p className="text-sm text-center mt-4">
+              Already Registered ??{" "}
+              <Link to="/login" className="font-medium text-primary underline ">
+                Create Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
