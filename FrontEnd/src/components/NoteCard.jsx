f4572104b256d3b984c0e2e387074abbe25f174c
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-30 20:12:23 +0530

    new-commit

---
+++ FrontEnd/src/components/NoteCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { MdOutlinePushPin } from "react-icons/md";
+import { MdCreate, MdDelete } from "react-icons/md";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tag,
+  isPlnned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) => {
+  return (
+    <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
+      <div className="flex items-center justify-between">
+        <div>
+          <h6 className="text-sm font-medium ">{title}</h6>
+          <span className="text-xs text-slate-600">{date}</span>
+        </div>
+        <MdOutlinePushPin
+          className={`icon-btn ${isPlnned ? "text-primary" : "text-slate-900"}`}
+          onClick={onPinNote}
+        />
+      </div>
+      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+      <div className="flex justify-between items-center">
+        <div className="text-xs text-slate-500 mt-2 ">{tag}</div>
+        <div className="flex gap-2 items-center ">
+          <MdCreate
+            className="icon-btn hover:text-green-600"
+            onClick={onEdit}
+          />
+          <MdDelete
+            className="icon-btn hover:text-red-600"
+            onClick={onDelete}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
+import NoteCard from "../components/NoteCard.jsx";
+import { IoMdAdd } from "react-icons/io";
+import AddEditNotes from "../components/AddEditNotes.jsx";
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className=""></div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8 rounded-md">
+          <NoteCard
+            title="Meeting on 7th December"
+            date="28th Nov 2024"
+            content="Important Meeting!"
+            tag="#Meeting"
+            isPlnned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+      <button className="w-16 h-16 flex items-center justify-center rounded-full bg-primary hover:bg-blue-900 absolute right-10 bottom-10 transition easy-in-out delay-50">
+        <IoMdAdd className="text-[32px] text-white" />
+      </button>
+
+      <AddEditNotes />
     </div>
   );
 };
