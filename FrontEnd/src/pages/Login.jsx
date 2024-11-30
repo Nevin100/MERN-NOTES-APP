79af3d1e69f2d8b84d6b3afdfa80842caa6db731
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-30 14:28:10 +0530

    new commit

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import { Link } from "react-router-dom";
-
+import PasswordInput from "../components/PasswordInput.jsx";
 const Login = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <Navbar />
 
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin()}>
             <h4 className="text-2xl mb-7">LogIn</h4>
-            <input type="text" placeholder="Email" className="input-box" />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <PasswordInput
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
             <button type="submit" className="btn-primary">
               Login
             </button>
