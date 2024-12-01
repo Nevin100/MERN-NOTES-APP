86d04edfa965b24d367d28f4f8b0abae5436c8c4
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-01 13:30:57 +0530

    new commits

--- FrontEnd/src/components/TagInput.jsx
+++ FrontEnd/src/components/TagInput.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdAdd, MdDelete } from "react-icons/md";
 
 const TagInput = ({ tags, setTags }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const AddNewTag = () => {
+    if (inputValue.trim() !== "") {
+      setTags([...tags, inputValue.trim()]);
+      setInputValue("");
+    }
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      AddNewTag();
+    }
+  };
+  const handleRemoveTag = (tagToRemove) => {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  };
   return (
     <div>
+      {tags?.length > 0 && (
+        <div className="flex items-center gap-2 flex-wrap mt-2">
+          {tags.map((tag, index) => (
+            <span key={index} className="">
+              #{tag}
+              <button
+                onClick={() => {
+                  handleRemoveTag(tag);
+                }}
+              >
+                <MdDelete />
+              </button>
+            </span>
+          ))}
+        </div>
+      )}
+
       <div className="flex items-center gap-4 mt-3">
         <input
           type="text"
           className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
           placeholder="Add tags"
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
         />
-
-        <button className="">
+        <button
+          className=""
+          onClick={() => {
+            AddNewTag();
+          }}
+        >
           <MdAdd className="text-2xl text-blue-700 hover:text-neutral-400" />
         </button>
       </div>
