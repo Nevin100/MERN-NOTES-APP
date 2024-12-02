64f266865fe7df7af7428938572325e558b26ad4
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-02 21:48:42 +0530

    new- commits

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -138,6 +138,8 @@ app.post("/login", async (req, res) => {
   }
 });
 
+//NOTES:
+
 // Add Note
 app.post("/add-note", authenticateToken, async (req, res) => {
   const { title, content, tags } = req.body;
@@ -235,6 +237,39 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
 
     await Note.deleteOne({ _id: noteId });
     return res.json({ error: false, message: "Note deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal Server Error" });
+  }
+});
+
+//Update isPinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { isPinned } = req.body;
+  const { user } = req.user;
+
+  if (isPinned === undefined) {
+    return res
+      .status(400)
+      .json({ error: true, message: "No changes provided" });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user.userId });
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    if (isPinned !== undefined) note.isPinned = isPinned;
+
+    await note.save();
+    return res.json({
+      error: false,
+      note,
+      message: "Pinned updated successfully",
+    });
   } catch (error) {
     return res
       .status(500)
